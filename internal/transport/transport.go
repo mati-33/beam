adb8cee73a61c9f2e3a327e0ee5cd5f08c3c7074
mati <mateusz.meksula@gmail.com>
2025-11-18 21:11:35 +0100

    transport

--- internal/transport/transport.go
+++ internal/transport/transport.go
@@ -1,1 +1,43 @@
 package transport
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+
+	p "github.com/mati-33/beam/internal/protocol"
+)
+
+func WriteMessage(dst io.Writer, m p.Message) error {
+	b := p.Encode(m)
+	_, err := io.Copy(dst, bytes.NewReader(b))
+	if err != nil {
+		return fmt.Errorf("failed to write message: %v", err)
+	}
+	return nil
+}
+
+func ReadMessage(src io.Reader) (p.Message, error) {
+	header := make([]byte, 7)
+
+	_, err := io.ReadFull(src, header)
+	if err != nil {
+		return p.Message{}, fmt.Errorf("failed to read message header: %v", err)
+	}
+
+	payloadLen := binary.BigEndian.Uint16(header[5:7])
+
+	rest := make([]byte, payloadLen+4)
+	_, err = io.ReadFull(src, rest)
+	if err != nil {
+		return p.Message{}, fmt.Errorf("failed to read message payload: %v", err)
+	}
+
+	m, err := p.Decode(append(header, rest...))
+	if err != nil {
+		return p.Message{}, fmt.Errorf("failed to decode message bytes: %v", err)
+	}
+
+	return m, nil
+}
